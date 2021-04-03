380461a31a7656611bfb3d65b738b00fb4d03cbe
Emma Almström <emma.almstrom@skola.botkyrka.se>
2021-04-03 10:54:58 +0200

    collision improved so you can't go trough the wall and the counter for walltouches goes down

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 function collision(centerX, centerY, map)
     if map[math.floor((centerY)/100)][math.floor((centerX)/100)] == 1 then
-        --love.window.showMessageBox(":(", "You lost !!!", "info" )
-        wallTouched = wallTouched + 1
+        
+        wallTouched = wallTouched - 1
         --ballX = 150
         --ballY = 250
+
     end
 end
 
@@ -13,15 +14,20 @@ function love.load()
     ballRadius = 30
     speed = 4
     message = "Go !"
-    wallTouched = 0
+    wallTouched = 100
     start = love.timer.getTime()
+    movingright = false
+    movingleft = false
+    movingup = false
+    movingdown = false
+
     
     grid = {
         {1, 1, 1, 1, 1, 1, 1, 1},
-        {0, 0, 1, 1, 1, 0, 0, 0},
-        {1, 0, 0, 1, 0, 0, 1, 1},
-        {1, 1, 0, 1, 0, 1, 1, 1},
-        {1, 1, 0, 0, 0, 1, 1, 1},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {1, 1, 0, 0, 0, 0, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1, 1, 1, 1}
     }
     for i = 1, 360, 10 do
@@ -63,24 +69,50 @@ end
 function love.update(dt)
     if love.keyboard.isDown("right") then
         ballX = ballX + speed
+        movingright = true
     end
     if love.keyboard.isDown("left") then
         ballX = ballX - speed
+        movingleft = true
     end
     if love.keyboard.isDown("down") then
         ballY = ballY + speed
+        movingdown = true
     end
     if love.keyboard.isDown("up") then
         ballY = ballY - speed
+        movingup = true
     end
-    -- kallar på collision så många gånger som circlePoints är
+    -- calls the collision-function for every table in my 'circlePoints'- table
     for index, value in ipairs(circlePoints) do
         collision(ballX + circlePoints[index][1], ballY + circlePoints[index][2], grid)
     end
-    -- vinst
+    -- victory
+    -- set a variable that describes delta between the time right now and the time the game started
     result = love.timer.getTime() - start
     if ballX > love.graphics.getWidth() + 70 then
         love.window.showMessageBox(":)", "You won! It took you " .. math.floor(result) .. " seonds", "info")
+        love.load()
+    end
+
+    -- stop the ball from clipping the wall
+    -- if the ball in on a 1 on the gridsystem and the ball moves in a specific direction
+    -- then the movment is nullified
+    if grid[math.floor((ballY)/100)][math.floor((ballX + 29)/100)] == 1 and movingright == true then
+        ballX = ballX - 4
+    end
+    if grid[math.floor((ballY)/100)][math.floor((ballX - 29)/100)] == 1 and movingleft == true then
+        ballX = ballX + 4
+    end
+    if grid[math.floor((ballY - 29)/100)][math.floor((ballX)/100)] == 1 and movingup == true then
+        ballY = ballY + 4
+    end
+    if grid[math.floor((ballY + 29)/100)][math.floor((ballX)/100)] == 1 and movingdown == true then
+        ballY = ballY - 4
+    end
+
+    if wallTouched == -1 then
+        love.window.showMessageBox(":(", "You lost !!!", "info" )
         love.load()
     end
 end
